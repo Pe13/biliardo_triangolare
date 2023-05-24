9961f2def53611545bbf6320576e22a21b872849
Pe13 <paolo.peforni@gmail.com>
2023-05-24 20:36:08 +0200

    App class methods definition moved to App.cpp and added draw method that simply clear and update the window

--- App.cpp
+++ App.cpp
@@ -5,4 +5,32 @@
 #include "App.hpp"
 
 namespace bt {
-}// namespace bt
+
+App::App(double l, double r1, double r2)
+    : window_{{800, 600}, "Biliardo trinagolare"}, l_{l}, r1_{r1}, r2_{r2} {
+  // limitiamo gli fps per far avanzare più facilmente la pallina a velocità
+  // costante
+  window_.setFramerateLimit(30);
+}
+
+void App::handleEvent() {
+  while (window_.pollEvent(event_)) {
+    if (event_.type == sf::Event::Closed) {
+      window_.close();
+    }
+  }
+}
+
+void App::draw() {
+  window_.clear(sf::Color::White);
+
+  window_.display();
+}
+
+void App::run() {
+  while (window_.isOpen()) {
+    handleEvent();
+  }
+}
+
+}  // namespace bt
