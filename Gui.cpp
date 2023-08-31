7175fda8f8e44a6991c7ac62a5d9fe461f4ac9b1
paolo <paolo.peforni@gmail.com>
2023-09-01 01:45:34 +0200

    Ultimata l'implementazione della gui. Aggiunta la funzione format per far sì che il metodo tgui::String::attemptToFloat accetti e rifiuti quello che voglio.

--- Gui.cpp
+++ Gui.cpp
@@ -4,37 +4,56 @@
 
 #include "Gui.hpp"
 
+#include <algorithm>
 #include <cmath>
 
 #include "App.hpp"
 
 namespace bt {
+// TODO
+//  1 - proteggersi dagli overflow negli input limitando il numero di caratteri e passando prima da un uint
 
-//template <typename Target>
-//void recursiveSearch(const tgui::Widget::Ptr &ptr, std::function<void(std::shared_ptr<Target>)> &fun) {
-//  if (ptr->getWidgetType() == tgui::HorizontalLayout::StaticWidgetType) {
-//    auto layout = std::static_pointer_cast<tgui::HorizontalLayout>(ptr);
-//    for (auto &elt : layout->getWidgets()) {
-//      if (elt->getWidgetType() == Target::StaticWidgetType) {
-//        auto target = std::static_pointer_cast<Target>(elt);
-//        fun(target);
-//      }
-//      recursiveSearch(elt, fun);
-//    }
-//  } else if (ptr->getWidgetType() == tgui::VerticalLayout::StaticWidgetType) {
-//    auto layout = std::static_pointer_cast<tgui::VerticalLayout>(ptr);
-//    for (auto &elt : layout->getWidgets()) {
-//      if (elt->getWidgetType() == Target::StaticWidgetType) {
-//        auto target = std::static_pointer_cast<Target>(elt);
-//        fun(target);
-//      }
-//      recursiveSearch(elt, fun);
-//    }
-//  } else if (ptr->getWidgetType() == Target::StaticWidgetType) {
-//    auto target = std::static_pointer_cast<Target>(ptr);
-//    fun(target);
-//  }
-//}
+// funzione per formattare le stringe di input numerici in modo da invalidarle o renderle comprensibili agli occhi del
+// metodo tgui::String::attemptToFloat
+tgui::String format(const tgui::String& str) {
+  if (str.empty()) {
+    return str;
+  }
+  auto string = str;
+
+  // rimuovo tutti gli apici e gli spazi (che possono essere usati per contare meglio le cifre inserite
+  string.remove('\'');
+  string.remove(' ');
+
+  string = string.toLower();  // così facendo mi basta controllare "e"
+
+  // trasformo le virgole in punti così da accettare entrambe le notazioni
+  std::replace(string.begin(), string.end(), ',', '.');
+
+  // salvo il segno per dopo, se non c'è non ri aggiungerò nulla
+  tgui::String sign{""};
+  if (string.starts_with('+') || string.starts_with('-')) {
+    sign = string[0];
+    string.erase(string.begin());
+  }
+  // se contiene anche solo uno dei caratteri sottoelencati invalido la stringa
+  if (string.find_first_not_of("0123456789e.") != tgui::String::npos) {
+    return {""};
+  }
+  // reinserisco l'eventuale segno
+  string.insert(0, sign);
+
+  // se contiene più di un punto decimale o più di una "e" invalido la stringa
+  if (std::count(string.begin(), string.end(), 'e') > 1 || std::count(string.begin(), string.end(), '.') > 1) {
+    return {""};
+  }
+  // se la "e" è prima del punto invalido la stringa
+  if (string.find('e') < string.find('.') && string.find('.') != tgui::String::npos) {
+    return {""};
+  }
+
+  return string;
+}
 
 void Gui::create() {
   gui.add(wrapper, "wrapper");
@@ -57,11 +76,11 @@ void Gui::create() {
   wrapper->add(newBiliardoWrapper, 2.5, "newBiliardoWrapper");
   newBiliardoWrapper->addSpace(.05);
   newBiliardoWrapper->add(newBiliardoInputWrapper, .3, "newBiliardoInputWrapper");
-  newBiliardoInputWrapper->add(r1Label, .5, "r1Label");
+  newBiliardoInputWrapper->add(r1Label, .7, "r1Label");
   newBiliardoInputWrapper->add(r1Input, "r1Input");
-  newBiliardoInputWrapper->add(r2Label, .5, "r2Label");
+  newBiliardoInputWrapper->add(r2Label, .7, "r2Label");
   newBiliardoInputWrapper->add(r2Input, "r2Input");
-  newBiliardoInputWrapper->add(lLabel, .5, "lLabel");
+  newBiliardoInputWrapper->add(lLabel, .7, "lLabel");
   newBiliardoInputWrapper->add(lInput, "lInput");
   newBiliardoWrapper->addSpace(.2);
   newBiliardoWrapper->add(newBiliardoBtn, .4, "newBiliardoBtn");
@@ -118,7 +137,7 @@ void Gui::create() {
   navigateWrapper->addSpace(.1);
 }
 
-void Gui::activate(App *app) {
+void Gui::activate(App* app) {
   // ripristino il colore del testo di default all'ottenimento del focus nel caso questo sia diventato rosso a causa di
   // un dato non valido inserito
   r1Input->onFocus([this] { r1Input->getRenderer()->setTextColor(tgui::Color::White); });
@@ -141,44 +160,46 @@ void Gui::activate(App *app) {
   r1Input->setText(tgui::String(app->biliardo_.r1()));
   r2Input->setText(tgui::String(app->biliardo_.r2()));
   lInput->setText(tgui::String(app->biliardo_.l()));
-
   newBiliardoBtn->onPress([this, app] {
     float r1, r2, l;
 
-    if (r1Input->getText().attemptToFloat(r1) && r1 > 0) {
+    // cambio singolarmente i parametri se questi sono stati inseriti, controllando che siano validi
+    if (format(r1Input->getText()).attemptToFloat(r1) && r1 > 0) {
       app->biliardo_.r1(r1, false);
-      sigmaYInput->setDefaultText(tgui::String(r1 / 5));
+      sigmaYInput->setDefaultText(tgui::String(r1 / 5));  // aggiorno il testo placeholder della sigmaY di default
     } else {
       r1Input->getRenderer()->setTextColor(tgui::Color::Red);
     }
-    if (r2Input->getText().attemptToFloat(r2) && r2 > 0) {
+    if (format(r2Input->getText()).attemptToFloat(r2) && r2 > 0) {
       app->biliardo_.r2(r2, false);
     } else {
       r2Input->getRenderer()->setTextColor(tgui::Color::Red);
     }
-    if (lInput->getText().attemptToFloat(l) && l > 0) {
+    if (format(lInput->getText()).attemptToFloat(l) && l > 0) {
       app->biliardo_.l(l, false);
     } else {
       lInput->getRenderer()->setTextColor(tgui::Color::Red);
     }
 
     app->modifyBiliardo();
-
   });
 
+  // attivo i bottoni per navigare tra un lancio e l'altro
   previousLaunchBtn->onPress(
       [app] { app->designer_.previousLaunch(&app->singleLaunches_[app->biliardo_.type()].front()); });
   nextLaunchBtn->onPress([app] { app->designer_.nextLaunch(&app->singleLaunches_[app->biliardo_.type()].back()); });
   pauseBtn->onPress([app] { app->designer_.pause(); });
   reRunBtn->onPress([app] { app->designer_.reRun(); });
 
+  // attivo il bottone per i lanci singoli
   singleLaunchBtn->onPress([this, app] {
     float y, t;
     bool isY, isT;
 
-    app->singleLaunches_[app->biliardo_.type()].emplace_back();
+    app->singleLaunches_[app->biliardo_.type()].emplace_back();  // genero lo spazio per un nuovo lanci in memoria
 
-    if (heightInput->getText().attemptToFloat(y) && std::abs(y) <= app->biliardo_.r1()) {
+    // controllo quali input sono presenti
+    if (format(heightInput->getText()).attemptToFloat(y) && std::abs(y) <= app->biliardo_.r1()) {
       isY = true;
     } else if (heightInput->getText().empty()) {
       isY = false;
@@ -187,7 +208,7 @@ void Gui::activate(App *app) {
       return;
     }
 
-    if (angleInput->getText().attemptToFloat(t) && std::abs(t) <= M_PI / 2) {
+    if (format(angleInput->getText()).attemptToFloat(t) && std::abs(t) <= M_PI / 2) {
       isT = true;
     } else if (angleInput->getText().empty()) {
       isT = false;
@@ -196,6 +217,7 @@ void Gui::activate(App *app) {
       return;
     }
 
+    // gestisco i vari casi
     if (!isY && !isT) {  // se ne y ne teta sono indicate le genera entrambe
       app->biliardo_.launchForDrawing(app->singleLaunches_[app->biliardo_.type()].back());
     } else if (isY && isT) {  // se invece sono entrambe indicate le usa semplicemente
@@ -206,10 +228,10 @@ void Gui::activate(App *app) {
       app->biliardo_.launchForDrawingNoY(t, app->singleLaunches_[app->biliardo_.type()].back());
     }
 
-    app->designer_.setPoints(&app->singleLaunches_[app->biliardo_.type()].back());
+    app->designer_.setPoints(&app->singleLaunches_[app->biliardo_.type()].back());  // aggiorno il lancio da riprodurre
   });
 
-  // imposto i placeholder per gli editbox del lancio multiplo
+  // imposto i placeholder per gli editbox del lancio multiplo con i valori di default delle distribuzioni normali
   muYInput->setDefaultText("0");
   sigmaYInput->setDefaultText(tgui::String(app->biliardo_.r1() / 5));
   muTInput->setDefaultText("0");
@@ -217,35 +239,34 @@ void Gui::activate(App *app) {
   numberInput->setDefaultText("1'000'000");
 
   multipleLaunchBtn->onPress([this, app] {
-    float muY = 0, sigmaY = static_cast<float>(app->biliardo_.r1() / 5), muT = 0, sigmaT = M_PI / 8;
-    int N = 10e6;
+    // dichiaro e gestisco N come un float fino alla fine perché se no non funziona la sintassi con la e
+    float muY = 0, sigmaY = static_cast<float>(app->biliardo_.r1() / 5), muT = 0, sigmaT = M_PI / 8, N = 10e6;
 
-    app->multipleLaunches_[app->biliardo_.type()].clear();
+    app->multipleLaunches_[app->biliardo_.type()].clear();  // pulisco la memoria per un nuovo lancio
 
     // catena di if poco elegante ma il for loop non funzionava
-    if (!muYInput->getText().attemptToFloat(muY) && !muYInput->getText().empty()) {
+    // mi limito a verificare che gli input siano validi perché ho dei valori di default
+    if (!format(muYInput->getText()).attemptToFloat(muY) && !muYInput->getText().empty()) {
       muYInput->getRenderer()->setTextColor((tgui::Color::Red));
       return;
     }
-    if (!sigmaYInput->getText().attemptToFloat(sigmaY) && !sigmaYInput->getText().empty()) {
+    if (!format(sigmaYInput->getText()).attemptToFloat(sigmaY) && !sigmaYInput->getText().empty()) {
       sigmaYInput->getRenderer()->setTextColor((tgui::Color::Red));
       return;
     }
-    if (!muTInput->getText().attemptToFloat(muT) && !muTInput->getText().empty()) {
+    if (!format(muTInput->getText()).attemptToFloat(muT) && !muTInput->getText().empty()) {
       muTInput->getRenderer()->setTextColor((tgui::Color::Red));
       return;
     }
-    if (!sigmaTInput->getText().attemptToFloat(sigmaT) && !sigmaTInput->getText().empty()) {
+    if (!format(sigmaTInput->getText()).attemptToFloat(sigmaT) && !sigmaTInput->getText().empty()) {
       sigmaTInput->getRenderer()->setTextColor((tgui::Color::Red));
       return;
     }
-    if (!numberInput->getText().attemptToInt(N) && !numberInput->getText().empty()) {
+    if (!format(numberInput->getText()).attemptToFloat(N) && !numberInput->getText().empty()) {
       numberInput->getRenderer()->setTextColor((tgui::Color::Red));
       return;
     }
 
-    // TODO fare parsing della stringa prima dell'analisi (invalidare stringhe con lettere in mezzo e accettare apici
-    // per separare le cifre)
     // controllo che le deviazioni standard non siano negative o nulle
     if (sigmaY <= 0) {
       sigmaYInput->getRenderer()->setTextColor(tgui::Color::Red);
@@ -256,11 +277,12 @@ void Gui::activate(App *app) {
       return;
     }
 
-    app->biliardo_.multipleLaunch(muY, sigmaY, muT, sigmaT, N, app->multipleLaunches_[app->biliardo_.type()]);
+    app->biliardo_.multipleLaunch(muY, sigmaY, muT, sigmaT, static_cast<int>(N),
+                                  app->multipleLaunches_[app->biliardo_.type()]);
 
     app->designer_.setCanvas(app->biliardo_.r1(), app->multipleLaunches_[app->biliardo_.type()], app->window_);
   });
 }
 
-Gui::Gui(sf::RenderWindow &window) : gui{window} { create(); }
+Gui::Gui(sf::RenderWindow& window) : gui{window} { create(); }
 }  // namespace bt
