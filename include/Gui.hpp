70ffa3973824372d591781bd9f2ed92ab0b7a289
paolo <paolo.peforni@gmail.com>
2023-10-31 00:36:49 +0100

    Creati i metodi per gestire la pressione dei bottoni che non sono gestiti da metodi della classe App, al posto delle lambda. Anche le altre lambda sono state sostituite con i puntatori alle funzioni. Create le funzioni forEachIndexed e forEach per eseguire una funzione su ogni elemento di un certo tipo dentro un container tgui, che hanno permesso di ridurre le linee di codice. C'è un leak dovuto al metodo onPress che devo capire se è colpa mia. Rivisto il funzionamento della funzione format, che ora restituisce una std::string per poter usare i metodi std::stod. Creata la funzione isValidInput che permette di gestire in maniera più comoda i controlli sulla validità degli input dell'utente

--- include/Gui.hpp
+++ include/Gui.hpp
@@ -15,14 +15,21 @@
 #include <TGUI/Widgets/Label.hpp>
 #include <TGUI/Widgets/TextArea.hpp>
 #include <TGUI/Widgets/VerticalLayout.hpp>
+#include <algorithm>
+#include <functional>
+#include <memory>
 
 namespace bt {
-
-// funzione per formattare le stringe di input numerici in modo da invalidarle o renderle comprensibili al metodo
-// tgui::String::attemptToFloat
-inline tgui::String format(const tgui::String& str) {
+/**
+ * @brief Formatta le stringe di input numerici in modo da invalidarle o renderle comprensibili al metodo
+ * std::stod
+ * @param str Stringa da formattare (non viene modificata)
+ * @return La stringa formattata ma in formato std::string così da poter essere usata con i metodi tipo std::stod.
+ * Oppure una stringa vuota nel caso non sia valida
+ */
+inline std::string format(const tgui::String& str) {
   if (str.empty()) {
-    return str;
+    return "";
   }
   auto string = str;
 
@@ -30,7 +37,7 @@ inline tgui::String format(const tgui::String& str) {
   string.remove('\'');
   string.remove(' ');
 
-  string = string.toLower();  // così facendo mi basta controllare "e"
+  string = string.toLower();  // così facendo mi basta controllare "e" e non "E"
 
   // trasformo le virgole in punti così da accettare entrambe le notazioni
   std::replace(string.begin(), string.end(), ',', '.');
@@ -43,21 +50,26 @@ inline tgui::String format(const tgui::String& str) {
   }
   tgui::String eSign{""};
   auto eSignPos = std::min(string.find("e+"), string.find("e-")) + 1;
-  if (eSignPos != 0) {
+  if (eSignPos != tgui::String::npos + 1) {
     eSign = string[eSignPos];
     string.erase(eSignPos, 1);
   }
 
+  // invalido la stringa se l'ultimo carattere è una e
+  if (string.back() == 'e') {
+    return "";
+  }
+
   // se contiene anche solo un carattere che non è tra quelli sottoelencati invalido la stringa
   if (string.find_first_not_of("0123456789e.") != tgui::String::npos) {
-    return {""};
+    return "";
   }
   // reinserisco l'eventuale segno del numero
   string.insert(0, sign);
 
   // se contiene più di un punto decimale o più di una "e" invalido la stringa
   if (std::count(string.begin(), string.end(), 'e') > 1 || std::count(string.begin(), string.end(), '.') > 1) {
-    return {""};
+    return "";
   }
   // reinserisco l'eventuale segno all'eventuale esponenziale
   eSignPos = string.find('e');
@@ -67,12 +79,98 @@ inline tgui::String format(const tgui::String& str) {
 
   // se la "e" è prima del punto invalido la stringa
   if (string.find('e') < string.find('.') && string.find('.') != tgui::String::npos) {
-    return {""};
+    return "";
   }
 
-  return string;
+  return string.toStdString();
 }
 
+/**
+ * @brief Ritorna true se la stringa è un input valido, e in tal caso salva il risultato sotto forma di double nella
+ * variabile fornita. In caso di fallimento la variabile non viene modificata
+ * @param string La stringa da esaminare
+ * @param val La variabile in cui salvare l'input
+ * @return True se la conversione è andata a buon fine, false altrimenti
+ */
+inline bool isValidInput(const tgui::String& string, double& val) {
+  try {
+    val = std::stod(format(string));
+    return true;
+  } catch (std::invalid_argument&) {
+    return false;
+  } catch (std::out_of_range&) {
+    return false;
+  }
+}
+
+using iterator = std::vector<tgui::Widget::Ptr>::const_iterator;
+/**
+ * @brief Applica una funzione lambda a ciascun elemento di un determinato tipo nell'intervallo specificato, tenendo
+ * traccia dell'indice.
+ *
+ * La funzione è ricorsiva e può gestire anche container nidificati all'interno dell'intervallo.
+ *
+ * @tparam T Il tipo di elemento a cui punta il puntatore al quale applicare la funzione lambda.
+ * @tparam Function Il tipo della funzione lambda.
+ * @param begin Un iteratore che punta all'inizio dell'intervallo.
+ * @param end Un iteratore che punta alla fine dell'intervallo.
+ * @param f La funzione lambda da applicare a ciascun elemento di tipo T.
+ * @param index L'indice iniziale per tenere traccia della posizione.
+ * @return L'indice dell'ultimo elemento processato nell'intervallo.
+ *
+ * Questa funzione template accetta un intervallo [begin, end) di tgui::Widget::Ptr e applica la funzione lambda fornita
+ * a ciascun elemento all'interno dell'intervallo se questo può essere convertito in un puntatore del tipo specificato,
+ * tenendo traccia dell'indice dell'elemento. La funzione lambda deve avere il seguente prototipo: `void lambda(const
+ * std::shared_ptr<T>& element, unsigned int index)`.
+ */
+template <typename T, typename Function>
+unsigned int forEachIndexed(iterator begin, iterator end, Function&& f, unsigned int index = 0) {
+  std::for_each(begin, end, [&](tgui::Widget::Ptr ptr) {
+    if (ptr->isContainer()) {
+      tgui::Container::Ptr container = std::static_pointer_cast<tgui::Container>(
+          ptr);  // posso usare uno static_cast dato che sono scuro sia un cast valido
+      auto children = container->getWidgets();
+      index = forEachIndexed<T>(children.begin(), children.end(), f, index);
+    }
+    auto target = std::dynamic_pointer_cast<T>(ptr);
+    if (target) {
+      f(target, index);
+      index++;
+    }
+  });
+  return index;
+}
+
+/**
+ * @brief Applica una funzione lambda a ciascun elemento di un determinato tipo nell'intervallo specificato.
+ *
+ * La funzione è ricorsiva e può gestire anche container nidificati all'interno dell'intervallo.
+ *
+ * @tparam T Il tipo di elemento a cui applicare la funzione lambda.
+ * @tparam Function Il tipo della funzione lambda.
+ * @param begin Un iteratore che punta all'inizio dell'intervallo.
+ * @param end Un iteratore che punta alla fine dell'intervallo.
+ * @param f La funzione lambda da applicare a ciascun elemento.
+ *
+ * Questa funzione template accetta un intervallo [begin, end) e applica la funzione lambda fornita a ciascun elemento
+ * all'interno dell'intervallo se questo può essere convertito in un puntatore del tipo specificato. La funzione lambda
+ * deve avere il seguente prototipo: `void lambda(const std::shared_ptr<T>& element)`.
+ */
+template <typename T, typename Function>
+void forEach(iterator begin, iterator end, Function&& f) {
+  std::for_each(begin, end, [&](tgui::Widget::Ptr ptr) {
+    if (ptr->isContainer()) {
+      tgui::Container::Ptr container = std::static_pointer_cast<tgui::Container>(
+          ptr);  // posso usare uno static_cast dato che sono scuro sia un cast valido
+      auto children = container->getWidgets();
+      forEach<T>(children.begin(), children.end(), f);
+    }
+    auto target = std::dynamic_pointer_cast<T>(ptr);
+    if (target) {
+      f(target);
+    }
+  });
+}
 
 class App;
 
@@ -145,14 +243,18 @@ struct Gui {
   tgui::TextArea::Ptr leftText{tgui::TextArea::create()};
   tgui::TextArea::Ptr rightText{tgui::TextArea::create()};
 
-  Gui(sf::RenderWindow& window);
+  explicit Gui(sf::RenderWindow& window);
+
+  void newBiliardoBtnPressed(App* app) const;
+  void singleLaunchBtnPressed(App* app) const;
+  void multipleLaunchBtnPressed(App* app) const;
 
   void create();
-  void style();
-  void activate(App* app);
-  void setDefaultText();
-  void setSingleLaunchText(const std::vector<double>& launch);
-  void setStatisticsText(const std::array<TH1D, 2>& histograms);
+  void style() const;
+  void activate(App* app) const;
+  void setDefaultText() const;
+  void setSingleLaunchText(const std::vector<double>& launch) const;
+  void setStatisticsText(const std::array<TH1D, 2>& histograms) const;
 };
 
 }  // namespace bt
