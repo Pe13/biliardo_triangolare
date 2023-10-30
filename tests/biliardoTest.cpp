87754a4cc41df634cbb131c9219718a1ced3acad
paolo <paolo.peforni@gmail.com>
2023-10-30 22:41:08 +0100

    Implementato l'uso della classe TBenchmark per la misura e il log dei tempi di esecuzione.

--- tests/biliardoTest.cpp
+++ tests/biliardoTest.cpp
@@ -2,8 +2,10 @@
 // Created by paolo on 14/06/2023.
 //
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <TBenchmark.h>
 #include <TH1D.h>
 
+#include <boost/implicit_cast.hpp>
 #include <chrono>
 #include <cmath>
 #include <execution>
@@ -14,56 +16,51 @@
 #include "doctest.h"
 
 TEST_CASE("Choosing the best method to launch multiple particles") {
+  TBenchmark benchmark;
+
   double r1 = 30;
   double r2 = 10;
   double l = 100;
 
-  float muY = 0, sigmaY = static_cast<float>(r1 / 5), muT = 0, sigmaT = M_PI / 8;
+  double muY = 0;
+  double sigmaY = r1 / 5;
+  double muT = 0;
+  double sigmaT = M_PI / 8;
   unsigned int N = 1e6;
 
   bt::Biliardo biliardo(l, r1, r2, bt::leftBounded);
 
   std::cout << "launching async...\n";
 
-  auto startAsync = std::chrono::high_resolution_clock::now();
-
   for (int _ = 0; _ < 10; _++) {
+    benchmark.Start("single async");
     std::array histograms = {TH1D("", "Istogramma delle y di uscita", 1000, -r1, r1),
                              TH1D("", "Istogramma degli angoli di uscita", 1000, -M_PI / 2, M_PI / 2)};
 
     biliardo.multipleLaunch(muY, sigmaY, muT, sigmaT, N, histograms);
+    benchmark.Show("single async");
   }
-
-  auto endAsync = std::chrono::high_resolution_clock::now();
-
-  auto AsyncDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endAsync - startAsync);
-
-  std::cout << "Async method took: " << AsyncDuration.count() << "ms\n";
 
   std::cout << "launching sync...\n";
 
-  auto startSync = std::chrono::high_resolution_clock::now();
+  for (int _ = 0; _ < 10; _++) {
+    benchmark.Start("single sync");
+    std::array histograms = {TH1D("", "Istogramma delle y di uscita", 1000, -r1, r1),
+                             TH1D("", "Istogramma degli angoli di uscita", 1000, -M_PI / 2, M_PI / 2)};
 
-    for (int _ = 0; _ < 10; _++) {
-      std::array histograms = {TH1D("", "Istogramma delle y di uscita", 1000, -r1, r1),
-                               TH1D("", "Istogramma degli angoli di uscita", 1000, -M_PI / 2, M_PI / 2)};
-
-      biliardo.syncMultipleLaunch(muY, sigmaY, muT, sigmaT, N, histograms);
-    }
-
-  auto endSync = std::chrono::high_resolution_clock::now();
-
-  auto SyncDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endSync - startSync);
-
-  std::cout << "Sync method took: " << SyncDuration.count() << "ms\n";
+    biliardo.syncMultipleLaunch(muY, sigmaY, muT, sigmaT, N, histograms);
+    benchmark.Show("single sync");
+  }
 }
 
 TEST_CASE("Choosing the best method to fill histograms") {
-  std::default_random_engine rng{static_cast<std::bernoulli_distribution::result_type>(
-      std::chrono::system_clock::now().time_since_epoch().count())};
+  std::default_random_engine rng{
+      boost::implicit_cast<unsigned long long>(std::chrono::system_clock::now().time_since_epoch().count())};
   std::uniform_real_distribution<double> dist{-1, 1};
 
-  auto startAsync = std::chrono::high_resolution_clock::now();
+  TBenchmark benchmark;
+
+  benchmark.Start("async");
 
   for (int _ = 0; _ < 100; _++) {
     std::array histograms = {TH1D("", "Istogramma delle y di uscita", 1000, -1, 1),
@@ -78,13 +75,9 @@ TEST_CASE("Choosing the best method to fill histograms") {
     });
   }
 
-  auto endAsync = std::chrono::high_resolution_clock::now();
+  benchmark.Show("async");
 
-  auto AsyncDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endAsync - startAsync);
-
-  std::cout << "Async method took: " << AsyncDuration.count() << "ms\n";
-
-  auto startSync = std::chrono::high_resolution_clock::now();
+  benchmark.Start("sync");
 
   for (int _ = 0; _ < 100; _++) {
     std::array histograms = {TH1D("", "Istogramma delle y di uscita", 1000, -1, 1),
@@ -94,17 +87,10 @@ TEST_CASE("Choosing the best method to fill histograms") {
     std::generate(v.begin(), v.end(), [&]() -> std::array<double, 2> { return {dist(rng), dist(rng)}; });
 
     std::for_each(v.begin(), v.end(), [&](const auto &arr) {
-
       histograms[0].Fill(arr[0]);
       histograms[1].Fill(arr[1]);
-
     });
   }
 
-  auto endSync = std::chrono::high_resolution_clock::now();
-
-  auto SyncDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endSync - startSync);
-
-  std::cout << "Sync method took: " << SyncDuration.count() << "ms\n";
-
+  benchmark.Show("sync");
 }
